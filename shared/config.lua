8e4ed428604f85fb314be6bd9afe18a9a24691c2
Nicole Heartstone <84972623+SerenaKing@users.noreply.github.com>
2022-10-04 07:25:41 +0200

    Update config.lua

--- shared/config.lua
+++ shared/config.lua
@@ -3,6 +3,6 @@ Config = {}
 Config.AllowedPanic = { "police", "bcso", "doc", "ambulance" }
 Config.AllowedMDT = { "police", "bcso", "doc", "ambulance" }
 
-Config.DISCORD_WEBHOOK = "https://ptb.discord.com/api/webhooks/1026176002654740490/UVDvDcvNRe0SGD0fTtILbp6Ykf8xBMzJsdefwVTCc08uczvtJGVfhx5k2BqojaqFLPtH"
+Config.DISCORD_WEBHOOK = ""
 Config.DISCORD_NAME = "LENT - Testing"
 Config.DISCORD_IMAGE = "https://cdn.discordapp.com/attachments/1026175982509506650/1026176123928842270/Lanzaned.png"
